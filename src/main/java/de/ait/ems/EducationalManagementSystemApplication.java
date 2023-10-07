5231bb4b2939068f9bfe00ca7caf7b73227145d8
Wladimir Weizen <weizen.w@gmx.de>
2023-10-07 18:20:52 +0200

    initial commit

---
+++ src/main/java/de/ait/ems/EducationalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package de.ait.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalManagementSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EducationalManagementSystemApplication.class, args);
+  }
+
+}
