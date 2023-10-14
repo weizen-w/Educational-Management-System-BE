10d5e9d47d5ea3a5fa2c5b4ceb6e237b54217fe2
Wladimir Weizen <weizen.w@gmx.de>
2023-10-14 18:30:00 +0200

    fix: replace @NotNull,@NotEmpty,@NotBlank with @Pattern and @Size

--- src/main/java/de/ait/ems/dto/NewCourseDto.java
+++ src/main/java/de/ait/ems/dto/NewCourseDto.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.Builder.Default;
 import lombok.Data;
 
 /**
--- src/main/java/de/ait/ems/dto/UpdateCourseDto.java
+++ src/main/java/de/ait/ems/dto/UpdateCourseDto.java
@@ -1,9 +1,7 @@
 package de.ait.ems.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,10 +18,8 @@ import lombok.NoArgsConstructor;
 @Schema(name = "Update course", description = "Data for updating the course")
 public class UpdateCourseDto {
 
-  @NotNull(message = "Must not be null")
-  @NotBlank(message = "Must not be blank")
-  @NotEmpty(message = "Must not be empty")
-  @Size(max = 100, message = "Size must be in the range from 0 to 100")
+  @Pattern(regexp = "^$|^(?!\\s+$).+", message = "Must not be blank or contain only spaces")
+  @Size(min = 1, max = 150, message = "Size must be in the range from 1 to 150")
   @Schema(description = "Course name", example = "Fullstack developer")
   private String name;
   @Schema(description = "Course is archived", example = "true")
