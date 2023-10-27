260f941c481b87cef1e05c647f84f142071e48ef
Wladimir Weizen <weizen.w@gmx.de>
2023-10-27 21:16:28 +0200

    fix: actual DB date

--- src/test/resources/sql/data.sql
+++ src/test/resources/sql/data.sql
@@ -18,22 +18,22 @@ values ('Course 3', false);
 insert into course(course_name,archived)
 values ('Course 4', false);
 
--- table group
-insert into group(group_name,course_id,archived)
+-- table student_group
+insert into student_group(group_name,course_id,archived)
 values ('Group 1', 1, false);
-insert into group(group_name,course_id,archived)
+insert into student_group(group_name,course_id,archived)
 values ('Group 2', 2, false);
-insert into group(group_name,course_id,archived)
+insert into student_group(group_name,course_id,archived)
 values ('Group 3', 3, false);
-insert into group(group_name,course_id,archived)
+insert into student_group(group_name,course_id,archived)
 values ('Group 4', 4, false);
 
 -- table student_group
-insert into student_group(student_id,group_id,main_group)
+insert into account_group(account_id,group_id,main_group)
 values (1, 1, true);
-insert into student_group(student_id,group_id,main_group)
+insert into account_group(account_id,group_id,main_group)
 values (2, 2, true);
-insert into student_group(student_id,group_id,main_group)
+insert into account_group(account_id,group_id,main_group)
 values (3, 3, true);
-insert into student_group(student_id,group_id,main_group)
+insert into account_group(account_id,group_id,main_group)
 values (4, 4, true);
