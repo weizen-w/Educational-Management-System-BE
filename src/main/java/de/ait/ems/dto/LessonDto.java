5934e1d62a96733e0f6134b8501a6b3ed9a1b74a
Oleksandr Zhurba <starkyzh@gmail.com>
2023-11-02 19:21:31 +0100

    feat: add Lesson dto`s, controller, api, service, repository

---
+++ src/main/java/de/ait/ems/dto/LessonDto.java
@@ -0,0 +1,76 @@
+package de.ait.ems.dto;
+
+import de.ait.ems.models.Lesson;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Oleksandr Zhurba on 01.11.2023.
+ * @project Educational-Management-System-BE
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(name = "Lesson", description = "Lessons for students and teachers")
+public class LessonDto {
+
+  @Schema(description = "Lesson ID", example = "1")
+  private Long lessonId;
+  @Schema(description = "Title", example = "basic_programming/lesson_52")
+  private String lessonTitle;
+  @Schema(description = "Description", example = "\n"
+      + "    Smalltalk about homework. Questions.\n"
+      + "    Binary seek in arrays\n"
+      + "    Binary seek by answer: collaboration task\n"
+      + "\n")
+  private String lessonDescription;
+  @Schema(description = "Lesson type", example = "consultation")
+  private String lessonType;
+  @Schema(description = "Teacher", implementation = UserDto.class)
+  private UserDto teacher;
+  @Schema(description = "Date", example = "2023-11-27")
+  private LocalDate lessonDate;
+  @Schema(description = "Start time", example = "10:15:30")
+  private LocalTime startTime;
+  @Schema(description = "End time", example = "12:15:30")
+  private LocalTime endTime;
+  @Schema(description = "Module", implementation = ModuleDto.class)
+  private ModuleDto module;
+  @Schema(description = "Link LMS", example = "https://lms.ait-tr.de/#/group/cohort26/module/basic_programming/lesson/lesson_52")
+  private String linkLms;
+  @Schema(description = "Link Zoom", example = "https://us05web.zoom.us/j/82073564366?pwd=LqOp9ojsiqseacSVwFPyt2jZA2tqUI.1")
+  private String linkZoom;
+  @Schema(description = "Archived", example = "false")
+  private Boolean archived;
+
+  public static LessonDto from(Lesson lesson) {
+    return LessonDto.builder()
+        .lessonId(lesson.getId())
+        .lessonTitle(lesson.getLessonTitle())
+        .lessonDescription(lesson.getLessonDescription())
+        .lessonType(lesson.getLessonType())
+        .teacher(UserDto.from(lesson.getTeacher()))
+        .lessonDate(lesson.getLessonDate())
+        .startTime(lesson.getStartTime())
+        .endTime(lesson.getEndTime())
+        .module(ModuleDto.from(lesson.getModule()))
+        .linkLms(lesson.getLinkLms())
+        .linkZoom(lesson.getLinkZoom())
+        .archived(lesson.getArchived())
+        .build();
+  }
+
+  public static List<LessonDto> from(List<Lesson> lessons) {
+    return lessons.stream()
+        .map(LessonDto::from)
+        .collect(Collectors.toList());
+  }
+}
---
+++ src/main/java/de/ait/ems/services/LessonService.java
@@ -0,0 +1,97 @@
+package de.ait.ems.services;
+
+import de.ait.ems.dto.LessonDto;
+import de.ait.ems.dto.NewLessonDto;
+import de.ait.ems.dto.UpdateLessonDto;
+import de.ait.ems.exceptions.RestException;
+import de.ait.ems.models.Lesson;
+import de.ait.ems.repositories.LessonRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Oleksandr Zhurba on 01.11.2023.
+ * @project Educational-Management-System-BE
+ **/
+@Service
+public class LessonService {
+
+  @Autowired
+  private UsersService usersService;
+  @Autowired
+  private LessonRepository lessonRepository;
+  @Autowired
+  private ModulesService modulesService;
+
+  public LessonDto addLesson(NewLessonDto newLesson, Long groupId) {
+    Lesson lesson = Lesson.builder()
+        .lessonTitle(newLesson.getLessonTitle())
+        .lessonDescription(newLesson.getLessonDescription())
+        .lessonType(newLesson.getLessonType())
+        .teacher(usersService.getUserOrThrow(newLesson.getTeacher().getId()))
+        .lessonDate(newLesson.getLessonDate())
+        .startTime(newLesson.getStartTime())
+        .endTime(newLesson.getEndTime())
+        .module(modulesService.getModuleOrThrow(newLesson.getModule().getId()))
+        .linkLms(newLesson.getLinkLms())
+        .linkZoom(newLesson.getLinkZoom())
+        .build();
+    lessonRepository.save(lesson);
+    return LessonDto.from(lesson);
+  }
+
+  public List<LessonDto> getLessonByGroup(Long groupId) {
+    List<LessonDto> result = new ArrayList<>();
+    List<Lesson> lessonList = lessonRepository.findByGroupId(groupId);
+    lessonList.forEach(lesson -> result.add(LessonDto.from(lesson)));
+    return result;
+  }
+
+  public LessonDto updateLesson(UpdateLessonDto updateLesson, Long lessonId) {
+    Lesson lessonForUpdate = getLessonOrThrow(lessonId);
+    if (updateLesson.getLessonTitle() != null) {
+      lessonForUpdate.setLessonTitle(updateLesson.getLessonTitle());
+    }
+    if (updateLesson.getLessonDescription() != null) {
+      lessonForUpdate.setLessonDescription(updateLesson.getLessonDescription());
+    }
+    if (updateLesson.getLessonType() != null) {
+      lessonForUpdate.setLessonType(updateLesson.getLessonType());
+    }
+    if (updateLesson.getTeacher() != null) {
+      lessonForUpdate.setTeacher(usersService.getUserOrThrow(updateLesson.getTeacher().getId()));
+    }
+    if (updateLesson.getLessonDate() != null) {
+      lessonForUpdate.setLessonDate(updateLesson.getLessonDate());
+    }
+    if (updateLesson.getStartTime() != null) {
+      lessonForUpdate.setStartTime(updateLesson.getStartTime());
+    }
+    if (updateLesson.getEndTime() != null) {
+      lessonForUpdate.setEndTime(updateLesson.getEndTime());
+    }
+    if (updateLesson.getModule() != null) {
+      lessonForUpdate.setModule(modulesService.getModuleOrThrow(updateLesson.getModule().getId()));
+    }
+    if (updateLesson.getLinkLms() != null) {
+      lessonForUpdate.setLinkLms(updateLesson.getLinkLms());
+    }
+    if (updateLesson.getLinkZoom() != null) {
+      lessonForUpdate.setLinkZoom(updateLesson.getLinkZoom());
+    }
+    if (updateLesson.getArchived() != null) {
+      lessonForUpdate.setArchived(updateLesson.getArchived());
+    }
+    lessonRepository.save(lessonForUpdate);
+    return LessonDto.from(lessonForUpdate);
+  }
+
+  public Lesson getLessonOrThrow(Long lessonId) {
+    return lessonRepository.findById(lessonId).orElseThrow(
+        () -> new RestException(HttpStatus.NOT_FOUND,
+            "Lesson with id <" + lessonId + "> not found"));
+  }
+}
