0134aa0fcc788a49e77fcbc7d87d009f0840eb3e
Oleksandr Zhurba <starkyzh@gmail.com>
2023-11-07 16:03:52 +0100

    feat: get Attendance list by auth user.

--- src/main/java/de/ait/ems/services/AttendanceService.java
+++ src/main/java/de/ait/ems/services/AttendanceService.java
@@ -5,7 +5,11 @@ import de.ait.ems.dto.UpdateAttendanceDto;
 import de.ait.ems.exceptions.RestException;
 import de.ait.ems.mapper.EntityMapper;
 import de.ait.ems.models.Attendance;
+import de.ait.ems.models.User;
 import de.ait.ems.repositories.AttendanceRepository;
+import de.ait.ems.security.details.AuthenticatedUser;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,6 +24,7 @@ public class AttendanceService {
 
   private final EntityMapper entityMapper;
   private final AttendanceRepository attendanceRepository;
+  private final UsersService usersService;
 
   public AttendanceDto updateAttendance(Long attendanceId, UpdateAttendanceDto updateAttendance) {
     Attendance attendanceForUpdate = getAttendanceOrThrow(attendanceId);
@@ -37,5 +42,15 @@ public class AttendanceService {
     return attendanceRepository.findById(attendanceId).orElseThrow(
         () -> new RestException(HttpStatus.NOT_FOUND,
             "Attendance with id <" + attendanceId + "> not found"));
+  }
+
+  public List<AttendanceDto> getAttendancesByAuthUser(AuthenticatedUser user) {
+    User userEntity = usersService.getUserOrThrow(user.getId());
+    if (userEntity != null) {
+      return attendanceRepository.getAttendanceByStudent(userEntity).stream()
+          .map(entityMapper::convertToDto)
+          .collect(Collectors.toList());
+    }
+    return null;
   }
 }
